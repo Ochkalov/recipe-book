9e767cd681fdb38d5aaf9f3832075a85fd4ea0d0
Ochkalov <ochkalov@gmail.com>
2017-07-31 21:59:50 +0300

    Recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
